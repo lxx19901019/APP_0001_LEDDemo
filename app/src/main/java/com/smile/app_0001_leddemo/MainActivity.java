22b03db1f6c11599ce35742af8317a7e553d7858
smile <862283034@qq.com>
2016-12-03 11:15:11 +0800

    v3, add methods for button and checkbox

--- app/src/main/java/com/smile/app_0001_leddemo/MainActivity.java
+++ app/src/main/java/com/smile/app_0001_leddemo/MainActivity.java
@@ -4,13 +4,90 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean ledon = false;
+    private Button button = null;
+    private CheckBox led1 = null;
+    private CheckBox led2 = null;
+    private CheckBox led3 = null;
+    private CheckBox led4 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        led1 = (CheckBox) findViewById(R.id.LED1);
+        led2 = (CheckBox) findViewById(R.id.LED2);
+        led3 = (CheckBox) findViewById(R.id.LED3);
+        led4 = (CheckBox) findViewById(R.id.LED4);
+
+        button = (Button) findViewById(R.id.BUTTON);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ledon = !ledon;
+                if(ledon) {
+                    button.setText("ALL OFF");
+
+                    led1.setChecked(true);
+                    led2.setChecked(true);
+                    led3.setChecked(true);
+                    led4.setChecked(true);
+                } else {
+                    button.setText("ALL ON");
+
+                    led1.setChecked(false);
+                    led2.setChecked(false);
+                    led3.setChecked(false);
+                    led4.setChecked(false);
+                }
+            }
+        });
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.LED1:
+                if (checked) {
+                    Toast.makeText(getApplicationContext(),"LED1 on", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"LED1 off", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.LED2:
+                if (checked) {
+                    Toast.makeText(getApplicationContext(),"LED2 on", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"LED2 off", Toast.LENGTH_SHORT).show();
+                }
+            case R.id.LED3:
+                if (checked) {
+                    Toast.makeText(getApplicationContext(),"LED3 on", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"LED3 off", Toast.LENGTH_SHORT).show();
+                }
+            case R.id.LED4:
+                if (checked) {
+                    Toast.makeText(getApplicationContext(),"LED4 on", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"LED4 off", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 
     @Override
